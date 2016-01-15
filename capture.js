84badd3f8cf6f377339ca8f80086fed6a4304721
Tim Perry <tim.perry@bbc.co.uk>
2016-01-15 23:29:06 +0000

    moving captured icons into their own folder

--- capture.js
+++ capture.js
@@ -1,9 +1,9 @@
 var casper = require('casper').create();
 
 casper.start('http://127.0.0.1:8371/', function() {
-	casper.captureSelector("540.png", ".fiveFourty");
-	casper.captureSelector("720.png", ".sevenTwenty");
-	casper.captureSelector( "1080.png", ".tenEighty");
+	casper.captureSelector("captures/540.png", ".fiveFourty");
+	casper.captureSelector("captures/720.png", ".sevenTwenty");
+	casper.captureSelector("captures/1080.png", ".tenEighty");
 });
 
 casper.run();
